cf166d31df1fd2d21eb296f49aa817c31115d061
Ricardo <ricardo.sabino@accedo.tv>
2022-08-10 03:20:22 -0400

    First commit with super simple image quantization

---
+++ MainForm.Designer.cs
@@ -0,0 +1,102 @@
+﻿
+namespace GBImgConverter
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.pbOriginal = new System.Windows.Forms.PictureBox();
+            this.pbGb = new System.Windows.Forms.PictureBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbOriginal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGb)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.pbOriginal, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pbGb, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(2194, 927);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // pbOriginal
+            // 
+            this.pbOriginal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbOriginal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbOriginal.Location = new System.Drawing.Point(3, 3);
+            this.pbOriginal.Name = "pbOriginal";
+            this.pbOriginal.Size = new System.Drawing.Size(1091, 921);
+            this.pbOriginal.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbOriginal.TabIndex = 0;
+            this.pbOriginal.TabStop = false;
+            // 
+            // pbGb
+            // 
+            this.pbGb.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbGb.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbGb.Location = new System.Drawing.Point(1100, 3);
+            this.pbGb.Name = "pbGb";
+            this.pbGb.Size = new System.Drawing.Size(1091, 921);
+            this.pbGb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbGb.TabIndex = 1;
+            this.pbGb.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2194, 927);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form_DragDrop);
+            this.DragOver += new System.Windows.Forms.DragEventHandler(this.Form_DragOver);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pbOriginal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGb)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.PictureBox pbOriginal;
+        private System.Windows.Forms.PictureBox pbGb;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GBImgConverter
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        Color[] GbPalette = new Color[] { Color.FromArgb(255, 169, 169, 169), Color.FromArgb(255, 128, 128, 128), Color.FromArgb(255, 90, 90, 90), Color.Black };
+        //Color[] GbPalette = new Color[] { Color.White, Color.LightGray, Color.DarkGray, Color.Black };
+
+        double DistanceToColour(Color c1, Color c2)
+        {
+            return Math.Sqrt((c1.R - c2.R) * (c1.R - c2.R) + (c1.G - c2.G) * (c1.G - c2.G) + (c1.B - c2.B) * (c1.B - c2.B));
+        }
+
+        public void Convert()
+        {
+            double[] distances = new double[GbPalette.Length];
+
+            var origImg = pbOriginal.Image; 
+
+            var GbImage = new Bitmap(origImg.Width, origImg.Height);
+
+            for (int x = 0; x < origImg.Width; x++)
+            {
+                for (int y = 0; y < origImg.Height; y++)
+                {
+                    Color oc = ((Bitmap)origImg).GetPixel(x, y);
+                    double md = double.MaxValue;
+                    var mi = 0;
+                    for(int i = 0; i < GbPalette.Length; i++)
+                    {
+                        var d = DistanceToColour(oc, GbPalette[i]);
+                        if(d < md)
+                        {
+                            md = d;
+                            mi = i;
+                        }
+                    }
+
+                    GbImage.SetPixel(x, y, GbPalette[mi]);
+                }
+            }
+
+            pbGb.Image = GbImage;
+        }
+
+        private void Form_DragOver(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Link;
+            else
+                e.Effect = DragDropEffects.None;
+        }
+
+        private void Form_DragDrop(object sender, DragEventArgs e)
+        {
+            // get all files droppeds  
+            if (e.Data.GetData(DataFormats.FileDrop) is string[] files && files.Any())
+            {
+                pbOriginal.Image = Image.FromFile(files.First());
+                Convert();
+            }
+        }
+    }
+}
